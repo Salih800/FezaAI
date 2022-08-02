6217e05fc03900986639ada3843639e71e34a9f0
Salih800 <salihcakmak700@gmail.com>
2022-08-02 15:49:26 +0300

    removed imports added log_detected_classes() arranged save_paths added self.save_detected_image small changes

--- src/object_detection_model.py
+++ src/object_detection_model.py
@@ -1,21 +1,35 @@
-import datetime
-import json
 import logging
 import time
 
 import requests
 
-from src.our_models import ModelInfo
-from src.constants import classes, landing_statuses
-from src.detected_object import DetectedObject
 import imutils
-import torch
 import os
-from PIL import Image, ImageDraw, ImageFont
 import cv2
 from sahi.model import DetectionModel
 from sahi.predict import get_prediction, get_sliced_prediction
 from sahi.prediction import PredictionResult
+
+
+def log_detected_classes(prediction, image_path):
+    arac, yaya, uap, uap_not, uai, uai_not = 0, 0, 0, 0, 0, 0
+    for detection in prediction.detected_objects:
+        if detection.cls == 0:
+            arac += 1
+        elif detection.cls == 1:
+            yaya += 1
+        elif detection.cls == 2:
+            if detection.landing_status == 1:
+                uap += 1
+            else:
+                uap_not += 1
+        elif detection.cls == 3:
+            if detection.landing_status == 1:
+                uai += 1
+            else:
+                uai_not += 1
+    logging.info(f"{image_path} Detected classes: {yaya} yaya, {arac} arac, "
+                 f"{uap} uap, {uap_not} uap_not, {uai} uai, {uai_not} uai_not")
 
 
 class ObjectDetectionModel:
@@ -24,10 +38,13 @@ class ObjectDetectionModel:
     def __init__(self, evaluation_server_url,
                  uap_uai_model: DetectionModel = None, uap_uai_sliced: bool = False,
                  yaya_arac_model: DetectionModel = None, yaya_arac_sliced: bool = False,
-                 view_image: bool = True):
+                 view_image: bool = True, save_detected_image: bool = True):
         logging.info('Created Object Detection Model')
         self.evaulation_server = evaluation_server_url
+
         self.images_folder = "./_images/"
+        self.detected_images_folder = "./_detected_images/"
+        self.detection_results_path = "./_detection_results_txt/"
 
         self.yaya_arac_model = yaya_arac_model
         self.uap_uai_model = uap_uai_model
@@ -36,6 +53,10 @@ class ObjectDetectionModel:
         self.yaya_arac_sliced = yaya_arac_sliced
 
         self.view_image = view_image
+        self.save_detected_image = save_detected_image
+
+        self.yaya_arac_model.model_name += "-sahi" if self.yaya_arac_sliced else ""
+        self.uap_uai_model.model_name += "-sahi" if self.uap_uai_sliced else ""
 
     @staticmethod
     def download_image(img_url, images_folder):
@@ -68,70 +89,47 @@ class ObjectDetectionModel:
         return frame_results
 
     def detect(self, prediction):
-        detection_results_path = "./_detection_results/" + prediction.video_name + "/"
-        if not os.path.isdir(detection_results_path):
-            os.makedirs(detection_results_path)
-
         image_path = self.images_folder[:-1] + prediction.image_url
         save_image_name = os.path.basename(prediction.image_url)[:-4]
-        # time_start = time.time()
+
         if self.yaya_arac_sliced:
             yaya_arac_result = get_sliced_prediction(image_path, self.yaya_arac_model)
         else:
             yaya_arac_result = get_prediction(image_path, self.yaya_arac_model)
 
-        # yaya_arac_result.export_visuals(export_dir="extracted_results/", file_name=save_image_name + "_result")
-        # logging.info(f"yaya-arac result seconds: {round(time.time() - time_start, 2)}")
-        # time_start = time.time()
-        # uap_uai_result = get_prediction(image_path, self.uap_uai_model)
         if self.yaya_arac_sliced:
             uap_uai_result = get_sliced_prediction(image_path, self.uap_uai_model)
         else:
             uap_uai_result = get_prediction(image_path, self.uap_uai_model)
-        # logging.info(f"uap-uai result seconds: {round(time.time() - time_start, 2)}")
 
-        prediction.detected_objects = yaya_arac_result.to_teknofest_predictions() + uap_uai_result.to_teknofest_predictions()
-        all_dedections = [] ############################
-        for detection in yaya_arac_result.object_prediction_list + uap_uai_result.object_prediction_list:
-            if detection.category.name.startswith("u"):
-                detection.category.id += 2
-            all_dedections.append(detection)
+        prediction.detected_objects = (yaya_arac_result.to_teknofest_predictions()
+                                       + uap_uai_result.to_teknofest_predictions())
 
-        all_detections = PredictionResult(
-            object_prediction_list=all_dedections,
-            image=yaya_arac_result.image,
-        )
+        if self.save_detected_image:
+            all_detections = []
+            for detection in yaya_arac_result.object_prediction_list + uap_uai_result.object_prediction_list:
+                if detection.category.name.startswith("u"):
+                    detection.category.id += 2
+                all_detections.append(detection)
+            all_detections = PredictionResult(
+                object_prediction_list=all_detections,
+                image=yaya_arac_result.image,
+            )
+            export_image_path = self.detected_images_folder + prediction.video_name + "/" \
+                                + self.yaya_arac_model.model_name + "_" + self.uap_uai_model.model_name + "/"
+            all_detections.export_visuals(export_dir=export_image_path, file_name=save_image_name)
 
-        all_detections.export_visuals(export_dir="extracted_results/", file_name=save_image_name + "_result")
-        if self.view_image:
-            cv2.imshow("img", imutils.resize(cv2.imread("extracted_results/" + save_image_name + "_result.png"), width=1280))
-            cv2.waitKey(1)
+            if self.view_image:
+                cv2.imshow("FRAME",
+                           imutils.resize(cv2.imread(export_image_path + save_image_name + ".png"), width=1280))
+                cv2.waitKey(1)
 
-        detections_filename = datetime.datetime.now().strftime(detection_results_path +
-                                                               os.path.basename(prediction.image_url)[:-4]
-                                                               + '__%Y_%m_%d__%H_%M_%S_%f.txt')
+            export_label_path = self.detection_results_path + prediction.video_name + "/" \
+                                + self.yaya_arac_model.model_name + "_" + self.uap_uai_model.model_name \
+                                + "/" + save_image_name + ".txt"
 
-        yaya_arac_result.save_yolo_label(detections_filename)
-        uap_uai_result.save_yolo_label(detections_filename)
-        # with open(detections_filename, "w") as all_json_file:
-        #     json.dump(all_json_results, all_json_file)
-        arac, yaya, uap, uap_not, uai, uai_not = 0, 0, 0, 0, 0, 0
-        for detection in prediction.detected_objects:
-            if detection.cls == 0:
-                arac += 1
-            elif detection.cls == 1:
-                yaya += 1
-            elif detection.cls == 2:
-                if detection.landing_status == 1:
-                    uap += 1
-                else:
-                    uap_not += 1
-            elif detection.cls == 3:
-                if detection.landing_status == 1:
-                    uai += 1
-                else:
-                    uai_not += 1
-        logging.info(f"Detected classes: {yaya} yaya, {arac} arac, "
-                     f"{uap} uap, {uap_not} uap_not, {uai} uai, {uai_not} uai_not")
+            all_detections.save_yolo_label(export_label_path)
+
+        log_detected_classes(prediction, image_path)
 
         return prediction
