c47e9d9e0c8dc9907afe664159831c368c139251
Salih800 <salihcakmak700@gmail.com>
2022-08-04 00:26:36 +0300

    added exception = "Model download failed!"

--- myutils/model_download.py
+++ myutils/model_download.py
@@ -7,7 +7,10 @@ def download_model(gdrive_id, download_path):
     if not os.path.isfile(download_path):
         os.makedirs(os.path.split(download_path)[0], exist_ok=True)
         logging.warning(f"Model not found in '{download_path}'! Trying to download it...")
-        gdown.download(id=gdrive_id, output=download_path, quiet=False)
+        if not gdown.download(id=gdrive_id, output=download_path, quiet=False):
+            exception = "Model download failed!"
+            logging.error(exception)
+            raise Exception(exception)
     else:
         logging.info(f"Model found in {download_path}: {gdown.md5sum(download_path)}")
 
